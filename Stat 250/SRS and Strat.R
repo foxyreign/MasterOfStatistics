1ced698b0efd41891e938c4a5a73755ba7ffb3a3
Adrian Cuyugan <adrian@foxyreign.com>
2017-02-25 15:57:05 +0800

    First upload

---
+++ Stat 250/SRS and Strat.R
@@ -0,0 +1,88 @@
+# Adrian Cuyugan - 91450
+
+## 1. Load dataset agpop.dat into R.
+setwd('D://Stat 250//')
+agpop <- read.csv('agpop.dat', head = T, sep = ',')
+
+## 2. Create a dartaframe in R from the dataset in (1) without any missing value for ACRES92.
+agpop <- subset(agpop, ACRES92 != -99)   			# Invalid cases
+agpop <- agpop.complete[!is.na(agpop $ACRES92),]	# Remove NAs 
+
+## 3. Create four (4) additional dataframes for each of the regions.
+agpop_ne <- subset(agpop, REGION == 'NE')
+agpop_nc <- subset(agpop, REGION == 'NC')
+agpop_s <- subset(agpop, REGION == 'S')
+agpop_w <- subset(agpop, REGION == 'W')
+
+## 4. For replication
+set.seed(91450)
+
+## 5. Obtain an SRS of size 300 freom the dataframe in (2).
+agpop.srs <- agpop[sample(nrow(agpop.complete), size = 300),]
+
+## 6. Obtain an estimate of the population mean using eqn (2.8).
+# For estimating the population mean y_bar from an SRS, use the sample mean.
+srs.mean <- mean(agpop.srs$ACRES92); print(srs.mean)
+
+# Variance estimation of SRS: (1-n/N) * (s^2/n)
+fpc <- (1-nrow(agpop.srs)/nrow(agpop))
+var.srs <- fpc * (sd(agpop.srs$ACRES92)/nrow(agpop.srs))
+print(var.srs)
+
+## 7. Obtain a 95% CI for the population mean using equations (2.12) and (2.22)
+# y_bar - t_(alpha/2,n-1)*(sqrt(fpc)*(var/sqrt(n)); y_bar + t_(alpha/2,n-1)*(sqrt(fpc)*(var/sqrt(n))
+
+se.srs <- sqrt(var.srs)				# Compute standard error
+t.low_95 <- qt(.025, df = 299, lower.tail = F)			# Compute t 95%, n-1	
+t.up_95 <- qt(.025, df = 299, lower.tail = F)	# Compute t 95%, n-1
+
+srs.low <- (srs.mean - t.low_95 * sqrt(fpc)) * (sqrt(var.srs)/sqrt(nrow(agpop.srs)))
+srs.up <- (srs.mean + t.up_95 * sqrt(fpc)) * (sqrt(var.srs)/sqrt(nrow(agpop.srs)))
+
+308709 - 1.96 * sqrt(1-300/3059) * sqrt(var.srs)/sqrt(300)
+308709 + 1.96 * sqrt(1-300/3059) * sqrt(var.srs)/sqrt(300)
+
+## 8. Obtain a stratified sample of size 300 from the dataframes from (3) allocated
+## according to the sizes mentioned in page 75 (Lohr).
+# NE = 21; NC = 103; S = 135; W = 41
+agstrat_ne <- agpop_ne[sample(nrow(agpop_ne), size = 21),]
+agstrat_nc <- agpop_nc[sample(nrow(agpop_nc), size = 103),]
+agstrat_s <- agpop_s[sample(nrow(agpop_s), size = 135),]
+agstrat_w <- agpop_w[sample(nrow(agpop_w), size = 41),]
+
+## 9. Obtain an estimate of the population mean using equation (3.2)
+# Compute for strata means
+strat_ne.mean <- mean(agstrat_ne$ACRES92); print(strat_ne.mean)   # Northeast      mean of ACRES92
+strat_nc.mean <- mean(agstrat_nc$ACRES92); print(strat_nc.mean)   # North Central  mean of ACRES92
+strat_s.mean <- mean(agstrat_s$ACRES92); print(strat_s.mean)      # South          mean of ACRES92
+strat_w.mean <- mean(agstrat_w$ACRES92); print(strat_w.mean)      # West           mean of ACRES92
+
+# Number of counties in stratum:
+northeast <- 220
+northcentral <- 1054
+south <- 1382
+west <- 422
+
+# Estimate mean population quantities of each stratum
+pop.mean_ne <- northeast * strat_ne.mean      # Northeast
+pop.mean_nc <- northcentral * strat_nc.mean   # North Central
+pop.mean_s <- south * strat_s.mean            # South
+pop.mean_w <- west * strat_w.mean             # West
+
+pop.mean_ne + pop.mean_nc + pop.mean_s + pop.mean_w
+
+# Compute for strata variances
+strat_ne.var <- (1-nrow(agstrat_ne)/northeast) * (sd(agstrat_ne$ACRES92)/nrow(agstrat_ne))
+strat_nc.var <- (1-nrow(agstrat_nc)/northcentral) * (sd(agstrat_nc$ACRES92)/nrow(agstrat_nc))
+strat_s.var <- (1-nrow(agstrat_s)/south) * (sd(agstrat_s$ACRES92)/nrow(agstrat_s))
+strat_w.var <- (1-nrow(agstrat_w)/west) * (sd(agstrat_w$ACRES92)/nrow(agstrat_w))
+
+## 10. Obtain a 95% CI for the population mean using the formulas on page 79 (Lohr)
+## using the percentile of the t-distribution with n-H df (n = 300, H = 4).
+
+
+
+
+
+
+
